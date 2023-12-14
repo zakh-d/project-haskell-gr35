08ec3cc237dd5a7125712fbcc65d11c8bcb0ea16
danylozakharchenko <danylo.zakharcehnko@gmail.com>
2023-12-14 14:51:34 +0100

    started modification app to run different programs

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,15 @@ module Main (main) where
 
 import Pandigital (returnBiggestHexDoublePandigital)
 
+
+program5 :: IO ()
+program5 = do
+    print "Enter n: "
+    n <- readLn
+    print(returnBiggestHexDoublePandigital (n :: Int))
+
 main :: IO ()
 main = do
-    n <- readLn
-    m <- returnBiggestHexDoublePandigital (n :: Int)
-    print m
+    print("Choose a program to run: [5/26/32]")
+
+    
