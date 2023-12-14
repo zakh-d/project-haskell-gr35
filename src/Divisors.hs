4c85a802d22909556c5fe494b02680b355ef91ed
danylozakharchenko <danylo.zakharcehnko@gmail.com>
2023-12-14 21:14:29 +0100

    divisors now is O(√n)

--- src/Divisors.hs
+++ src/Divisors.hs
@@ -6,7 +6,9 @@ isqrt :: Int -> Int
 isqrt = floor . sqrt . fromIntegral
 
 divisors :: Int -> Int
-divisors n = length [x | x <- [1..n], n `mod` x == 0]
+divisors n
+    | (sqrt (fromIntegral n) - fromIntegral(isqrt n)) == 0 = 2 * length [x | x <- [1..isqrt(n)], n `mod` x == 0] - 1
+    | otherwise = 2 * length [x | x <- [1..isqrt(n)], n `mod` x == 0]
 
 maxDivisorCount :: Int -> Int -> Int
 maxDivisorCount n k = maximum [divisors j | j <- [n..n+k-1]]
@@ -16,11 +18,10 @@ maxDivisorCount n k = maximum [divisors j | j <- [n..n+k-1]]
 
 sumM :: Int -> Int -> Int
 -- calculates sum using sliding window approach
-sumM u k = sumM' 1 (maxDivisorCount 1 k)
-    where
-        sumM' :: Int -> Int -> Int
-        sumM' n prev
-            | n > u - k + 1 = 0
-            | divisors (n + k - 1) > prev = prev + sumM' (n + 1) (divisors (n + k - 1))
-            | divisors (n - 1) == prev = prev + sumM' (n + 1) (maxDivisorCount n k)
-            | otherwise = prev + sumM' (n + 1) prev
+sumM u k = sumM' 2 (maxDivisorCount 1 k) (maxDivisorCount 1 k)
+  where
+    sumM' n prev _sum
+      | n >= u - k + 2 = _sum
+      | divisors (n + k - 1) > prev = sumM' (n + 1) (divisors (n + k - 1)) (_sum + divisors (n + k - 1))
+      | divisors (n - 1) == prev = sumM' (n + 1) (maxDivisorCount n k) (_sum + maxDivisorCount n k)
+      | otherwise = sumM' (n + 1) prev (_sum + prev)
