40d824e0e8afa97078e3a310dbab07d994ab41d5
danylozakharchenko <danylo.zakharcehnko@gmail.com>
2023-12-15 09:40:17 +0100

    returnBiggestHexDoublePandigital refactoring

--- src/Pandigital.hs
+++ src/Pandigital.hs
@@ -7,4 +7,4 @@ module Pandigital
 returnBiggestHexDoublePandigital :: Integer -> Integer
 returnBiggestHexDoublePandigital n
   | n > 15 = 0
-  | otherwise = foldl (\acc i -> acc * 16 + (n + 2 - i)) 0 (replicate 2 =<< [2..n + 2])
+  | otherwise = foldl (\acc i -> acc * 16 + (n - i)) 0 (replicate 2 =<< [0..n])
